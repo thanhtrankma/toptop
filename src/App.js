792552d0d0b9695170e3d906d9e782a908f61f23
Thanh Tran <thanhtrankma>
2024-12-11 16:03:11 +0700

    prettier

--- src/App.js
+++ src/App.js
@@ -3,29 +3,28 @@ import { useStore, actions } from './store';
 import { useRef } from 'react';
 
 function App() {
+  const [state, dispatch] = useStore();
+  const { todos, todoInput } = state;
 
-  const [state, dispatch] = useStore()
-  const { todos, todoInput } = state
-
-  const inputRef = useRef()
+  const inputRef = useRef();
 
   const handleAdd = () => {
-    dispatch(actions.addTodo(todoInput))
-    dispatch(actions.setTodoInput(''))
-    inputRef.current.focus()
-  }
+    dispatch(actions.addTodo(todoInput));
+    dispatch(actions.setTodoInput(''));
+    inputRef.current.focus();
+  };
   const handleDelete = (index) => {
-    dispatch(actions.deleteTodo(index))
-  }
+    dispatch(actions.deleteTodo(index));
+  };
 
   return (
     <div className="App">
       <input
         ref={inputRef}
         value={todoInput}
-        placeholder='Enter..'
-        onChange={e => {
-          dispatch(actions.setTodoInput(e.target.value))
+        placeholder="Enter.."
+        onChange={(e) => {
+          dispatch(actions.setTodoInput(e.target.value));
         }}
       />
       <button onClick={handleAdd}>ADD</button>
