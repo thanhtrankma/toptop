9c52b690feb7c29115e8c8427d9aa0e6d3c67c59
Thanh Tran <thanhtran.kma@gmail.com>
2025-02-03 11:19:02 +0700

    add layout

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
+import { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { publicRoutes } from '~/routes';
+import { DefaultLayout } from './components/layout';
+
 function App() {
   return (
-    <div className="App">
-      <h1>TOPTOP</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          {publicRoutes.map((route, index) => {
+            const Layout = route.layout === null ? Fragment : DefaultLayout;
+            const Page = route.component;
+            return (
+              <Route
+                key={index}
+                path={route.path}
+                element={
+                  <Layout>
+                    <Page />
+                  </Layout>
+                }
+              />
+            );
+          })}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
